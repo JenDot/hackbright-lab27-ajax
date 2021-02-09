9cd0ba3bc053e8cf474a5424b148a70a009524a9
Keiva <jen@silverwire.ninja>
2021-02-09 13:53:11 -0700

    Final Commit: completed Part 3.

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -30,12 +30,28 @@ $("#weather-form").on('submit', showWeather);
 
 
 // PART 3: ORDER MELONS
+function changeMelonLst(res) {
+    if (res.code === "OK") {
+        $('#order-status').html("<p>" + res.msg + "</p>");
+    }
+    else {
+        $('#order-status').addClass('order-error');
+        $('#order-status').html("<p>" + res.msg + "</p>")
+    }
+}
+
 
 function orderMelons(evt) {
     evt.preventDefault();
 
     // TODO: show the result message after your form
+    let formInput = {
+        'kind_of_melon': $("#melon-type-field").val(),
+        'quantity': $("#qty-field").val(),
+    };
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    $.post("/order-melons.json", formInput, changeMelonLst);
+
 }
 
 $("#order-form").on('submit', orderMelons);
